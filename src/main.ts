2a0ebb6170c9cafecccb37e138928e17c554f4a7
fastoch <fastoch@ik.me>
2025-07-01 20:35:47 +0200

    @18/56

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
 import { db } from "./drizzle/db"
+import { UserTable } from "./drizzle/schema"
 
 async function main() {
-  db
+  // add a new user to the database
+  await db.insert(UserTable).values({ 
+    name: "John Doe" 
+  })
+  // query the first user from the database
+  const firstUser = await db.query.UserTable.findFirst()
 }
 
 main()
